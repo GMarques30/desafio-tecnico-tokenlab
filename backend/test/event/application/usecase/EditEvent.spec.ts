e062ab92525cdbce27c634c350b2ce4aae9d2c29
Giovanni Marques <giovanni.marques030@gmail.com>
2024-12-24 18:03:50 -0300

    test: change in test parameters

--- backend/test/event/application/usecase/EditEvent.spec.ts
+++ backend/test/event/application/usecase/EditEvent.spec.ts
@@ -1,26 +1,21 @@
 import { ConflictError } from "../../../../src/account/application/errors/ConflictError";
 import { NotFoundError } from "../../../../src/account/application/errors/NotFoundError";
-import { AccountRepository } from "../../../../src/account/application/repository/AccountRepository";
 import { Account } from "../../../../src/account/domain/entity/Account";
 import { NotEventCreator } from "../../../../src/event/application/errors/NotEventCreator";
 import { EventRepository } from "../../../../src/event/application/repository/EventRepository";
 import { EditEvent } from "../../../../src/event/application/usecase/EditEvent";
 import { Event } from "../../../../src/event/domain/entity/Event";
-import { AccountRepositoryMemory } from "../../../account/infra/repository/AccountRepositoryMemory";
 import { EventRepositoryMemory } from "../../infra/repository/EventRepositoryMemory";
 
-let accountRepository: AccountRepository;
 let eventRepository: EventRepository;
 let sut: EditEvent;
 let account: Account;
 let event: Event;
 
 beforeEach(async () => {
-  accountRepository = new AccountRepositoryMemory();
   eventRepository = new EventRepositoryMemory();
   sut = new EditEvent(eventRepository);
   account = Account.create("John", "Doe", "john.doe@example.com", "John@123");
-  await accountRepository.save(account);
   event = Event.create(
     "Event 1",
     account.getAccountId(),
@@ -39,11 +34,11 @@ test("Should be possible to edit an event", async function () {
     finishedAt: "2026-01-03T00:00:00",
   };
   await sut.execute(input);
-  const acc = await eventRepository.findByEventId(input.eventId);
-  expect(acc).toBeDefined();
-  expect(acc?.getDescription()).toEqual(input.description);
-  expect(acc?.getStartedAt()).toEqual(new Date(input.startedAt));
-  expect(acc?.getFinishedAt()).toEqual(new Date(input.finishedAt));
+  const eventUpdated = await eventRepository.findByEventId(input.eventId);
+  expect(eventUpdated).toBeDefined();
+  expect(eventUpdated?.getDescription()).toEqual(input.description);
+  expect(eventUpdated?.getStartedAt()).toEqual(new Date(input.startedAt));
+  expect(eventUpdated?.getFinishedAt()).toEqual(new Date(input.finishedAt));
 });
 
 test("Should be possible to change some of the data", async function () {
@@ -53,17 +48,16 @@ test("Should be possible to change some of the data", async function () {
     accountId: account.getAccountId(),
   };
   await sut.execute(input);
-  const acc = await eventRepository.findByEventId(input.eventId);
-  expect(acc?.getDescription()).toEqual(input.description);
-  expect(acc?.getStartedAt()).toEqual(event.getStartedAt());
-  expect(acc?.getFinishedAt()).toEqual(event.getFinishedAt());
+  const eventUpdated = await eventRepository.findByEventId(input.eventId);
+  expect(eventUpdated?.getDescription()).toEqual(input.description);
+  expect(eventUpdated?.getStartedAt()).toEqual(event.getStartedAt());
+  expect(eventUpdated?.getFinishedAt()).toEqual(event.getFinishedAt());
 });
 
 test("Should throw an error if the event was not found", function () {
   const input = {
     eventId: crypto.randomUUID(),
     accountId: account.getAccountId(),
-    finishedAt: "2026-12-28T00:00:00",
   };
   expect(() => sut.execute(input)).rejects.toThrow(
     new NotFoundError("Event not found.")
@@ -71,18 +65,9 @@ test("Should throw an error if the event was not found", function () {
 });
 
 test("Should throw an error if the accountId does not match the event's accountId", async function () {
-  const newAccount = Account.create(
-    "John",
-    "Doe",
-    "john.doe2@example.com",
-    "John@123"
-  );
-  await accountRepository.save(newAccount);
   const input = {
     eventId: event.getEventId(),
-    startedAt: "2026-12-21T00:00:00",
-    finishedAt: "2026-12-22T00:00:00",
-    accountId: newAccount.getAccountId(),
+    accountId: crypto.randomUUID(),
   };
   expect(() => sut.execute(input)).rejects.toThrow(
     new NotEventCreator("You are not the creator of this event.")
