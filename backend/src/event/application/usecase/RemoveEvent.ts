3171352c41683c80ebde495d1f7bdaf1573bb144
Giovanni Marques <giovanni.marques030@gmail.com>
2024-12-24 09:43:44 -0300

    fix: remove account validation within the remove event use case

--- backend/src/event/application/usecase/RemoveEvent.ts
+++ backend/src/event/application/usecase/RemoveEvent.ts
@@ -1,28 +1,19 @@
 import { NotFoundError } from "../../../account/application/errors/NotFoundError";
-import { AccountRepository } from "../../../account/application/repository/AccountRepository";
+import { NotEventCreator } from "../errors/NotEventCreator";
 import { EventRepository } from "../repository/EventRepository";
 
 export class RemoveEvent {
-  private readonly accountRepository: AccountRepository;
   private readonly eventRepository: EventRepository;
 
-  constructor(
-    accountRepository: AccountRepository,
-    eventRepository: EventRepository
-  ) {
-    this.accountRepository = accountRepository;
+  constructor(eventRepository: EventRepository) {
     this.eventRepository = eventRepository;
   }
 
   async execute(input: RemoveEventInput) {
-    const account = await this.accountRepository.findByAccountId(
-      input.accountId
-    );
-    if (!account) throw new NotFoundError("Account not found.");
     const event = await this.eventRepository.findByEventId(input.eventId);
     if (!event) throw new NotFoundError("Event not found.");
-    if (event.getAccountId() !== account.getAccountId()) {
-      throw new Error("You are not the creator of this event.");
+    if (event.getAccountId() !== input.accountId) {
+      throw new NotEventCreator("You are not the creator of this event.");
     }
     this.eventRepository.remove(event.getEventId());
   }
--- backend/test/event/application/usecase/RemoveEvent.spec.ts
+++ backend/test/event/application/usecase/RemoveEvent.spec.ts
@@ -1,24 +1,20 @@
 import { NotFoundError } from "../../../../src/account/application/errors/NotFoundError";
-import { AccountRepository } from "../../../../src/account/application/repository/AccountRepository";
 import { Account } from "../../../../src/account/domain/entity/Account";
+import { NotEventCreator } from "../../../../src/event/application/errors/NotEventCreator";
 import { EventRepository } from "../../../../src/event/application/repository/EventRepository";
 import { RemoveEvent } from "../../../../src/event/application/usecase/RemoveEvent";
 import { Event } from "../../../../src/event/domain/entity/Event";
-import { AccountRepositoryMemory } from "../../../account/infra/repository/AccountRepositoryMemory";
 import { EventRepositoryMemory } from "../../infra/repository/EventRepositoryMemory";
 
 let eventRepository: EventRepository;
-let accountRepository: AccountRepository;
 let sut: RemoveEvent;
 let account: Account;
 let event: Event;
 
 beforeEach(async () => {
   eventRepository = new EventRepositoryMemory();
-  accountRepository = new AccountRepositoryMemory();
-  sut = new RemoveEvent(accountRepository, eventRepository);
+  sut = new RemoveEvent(eventRepository);
   account = Account.create("John", "Doe", "john.doe@example.com", "John@123");
-  await accountRepository.save(account);
   event = Event.create(
     "Event 1",
     account.getAccountId(),
@@ -38,16 +34,6 @@ test("Should be possible to remove an event", async function () {
   expect(eventData).toEqual(undefined);
 });
 
-test("Should throw an error if the account is not found", function () {
-  const input = {
-    eventId: event.getEventId(),
-    accountId: crypto.randomUUID(),
-  };
-  expect(() => sut.execute(input)).rejects.toThrow(
-    new NotFoundError("Account not found.")
-  );
-});
-
 test("Should throw an error if the event is not found", function () {
   const input = {
     eventId: crypto.randomUUID(),
@@ -59,18 +45,11 @@ test("Should throw an error if the event is not found", function () {
 });
 
 test("Should throw an error when you remove it if it wasn't that account that created the event", async function () {
-  const newAccount = Account.create(
-    "John",
-    "Doe",
-    "john.doe2@example.com",
-    "Doe@1234"
-  );
-  await accountRepository.save(newAccount);
   const input = {
     eventId: event.getEventId(),
-    accountId: newAccount.getAccountId(),
+    accountId: crypto.randomUUID(),
   };
   expect(() => sut.execute(input)).rejects.toThrow(
-    new Error("You are not the creator of this event.")
+    new NotEventCreator("You are not the creator of this event.")
   );
 });
